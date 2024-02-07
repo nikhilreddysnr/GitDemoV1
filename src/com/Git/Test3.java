dc95a3b798a63a253c91a1e884dc3bda2c782c5d
nikhilreddys <nikhilreddy4560@gmail.com>
2024-02-07 15:00:28 +0530

    Third Commit of Code through Branch 2

---
+++ src/com/Git/Test3.java
@@ -0,0 +1,18 @@
+package com.Git;
+
+public class Test3 
+{
+	public static void main(String[] args) {
+		String text = "Yes";
+		
+		if(text == "Yes")
+		{
+			System.out.println("Using Log4j2");
+		}
+		else
+		{
+			System.out.println("Incorrect");
+		}
+	}
+
+}
